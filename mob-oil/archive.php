31f1b9b04d34fa93117b930d1b25c7dca7a50688
Russ berge <russ@toplinemediateam.com>
2016-11-24 07:02:38 -0500

    site on dev server updates

--- mob-oil/archive.php
+++ mob-oil/archive.php
@@ -1,8 +1,41 @@
 <?php get_header(); ?>
+        <div class="oilDripLeft">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-1.png" alt="oildrip" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-2.png" alt="oildrip" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-3.png" alt="oildrip" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-4b.png" alt="oildrip" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-5.png" alt="oildrip" >
+        </div>
+
+        <div class="oilDripRight">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-5.png" alt="oildrip" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-4b.png" alt="oildrip" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-3.png" alt="oildrip" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-2.png" alt="oildrip" >
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drip-1.png" alt="oildrip" >
+        </div>
+
+        <div class="oilDrop-1">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drop.png" alt="oildrop" >
+        </div>
+        <div class="oilDrop-2">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drop.png" alt="oildrop" >
+        </div>
+        <div class="oilDrop-3">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drop.png" alt="oildrop" >
+        </div>
+        <div class="oilDrop-4">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drop.png" alt="oildrop" >
+        </div>  <div class="oilDrop-5">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drop.png" alt="oildrop" >
+        </div>  
+            <div class="oilDrop-6">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/drop.png" alt="oildrop" >
+        </div>
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="mainSection>
 
 			<h1><?php _e( 'Archives', 'html5blank' ); ?></h1>
 
@@ -14,6 +47,6 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
