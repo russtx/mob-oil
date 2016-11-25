3d825b97536400d8e5f761c7c91d01a04cd88e4f
Russ berge <russ@toplinemediateam.com>
2016-11-25 11:42:24 -0500

    updated

--- mob-oil/functions.php
+++ mob-oil/functions.php
@@ -57,7 +57,6 @@ if (function_exists('add_theme_support'))
     // Localisation Support
     load_theme_textdomain('html5blank', get_template_directory() . '/languages');
 }
-
 /*------------------------------------*\
 	Functions
 \*------------------------------------*/
